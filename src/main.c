7b5f3e59404da5ec2f5ee8c52738792ce137ebf7
Sebastian Muxel <sebastian@muxel.dev>
2024-10-26 22:48:19 +0200

    feat: add raylib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
+#include "raylib.h"
 
 int main(void)
 {
+    InitWindow(800, 800, "sand");
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
     return EXIT_SUCCESS;
 }
